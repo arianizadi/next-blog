8ef8d98940cc022cf0e21a8224dbcb133abdf4d9
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-07-04 18:51:39 -0700

    Update bio

--- components/BasicIntro.tsx
+++ components/BasicIntro.tsx
@@ -12,9 +12,10 @@ export function BasicIntro() {
       <Image src="/arian_fa22.jpg" alt="Arian Izadi Portrait" width={200} height={200} className="z-10 border-2 border-white rounded-full" />
       <div className="px-8 pt-10 text-center text-white">
         <h1 className="font-bold text-2xl">My name is Arian!</h1>
-        <p className="pt-2 max-w-xl">I am a senior studying Computer Science at the University of Nevada, Las Vegas.
-          In addition to my academic pursuits, I currently serve as the President of Layer Zero, the cybersecurity club on campus.
-          I am always eager to explore and create innovative solutions to problems.</p>
+        <p className="pt-2 max-w-xl">I have a passion for building and hacking things.
+          This website/blog is where I will post most of my current projects and tutorials that I decide to write.
+          Currently, I am a robotics software engineering intern, but I enjoy working with embedded systems,
+          reverse engineering, application security, networking, and web apps. Hopefully, I'll post something you find interesting!</p>
       </div>
     </div>
   )
