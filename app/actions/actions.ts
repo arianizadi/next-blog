5b3ac76e15ce856a87a6ebc04ebb9aea27a05f81
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-07-04 19:37:08 -0700

    Remove create test post

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -1,25 +1,6 @@
 "use server"
 
 import prisma from "@/prisma/db"
-import { revalidatePath } from "next/cache"
-
-export async function createTestPost() {
-
-  // await prisma.blogPost.deleteMany()
-
-  await prisma.blogPost.create({
-    data: {
-      title: "HelloWorld",
-      description: "First blog!",
-      tags: ["bruh"],
-      content: "Yeeee"
-    }
-  })
-
-  console.log("Created Test Post")
-
-  revalidatePath("/")
-}
 
 export async function getPosts() {
   const posts = await prisma.blogPost.findMany()
