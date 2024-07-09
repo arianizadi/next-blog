954d136365597a73de49d44d933c7ff42a12f95f
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-07-08 22:22:38 -0700

    Mobile Fix

--- app/blog/[post]/layout.tsx
+++ app/blog/[post]/layout.tsx
@@ -1,6 +1,6 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
-  return <div className="flex justify-center p-20">
+  return <div className="w-full justify-center p-10 md:flex">
     {children}
   </div>
 }
