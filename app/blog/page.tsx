ab93932ee4af0ad3f9d5d6742c5da8c3028bc0ab
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-07-13 04:01:19 -0700

    Fix Hydration

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,36 +12,30 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-
 export default async function Home() {
   const posts: BlogPost[] = await getPosts()
   return (
     <main className="p-4">
       <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {posts.map((post: BlogPost) => (
-        <Link key={post.id} href={"/blog/" + post.id}>
+          <Link key={post.id} href={"/blog/" + post.id}>
             <Card>
               <CardHeader className="items-center">
-              <CardTitle>{post.title}</CardTitle>
+                <CardTitle>{post.title}</CardTitle>
                 <CardDescription className="flex flex-col items-center pt-5">
-                  <p>{post.description}</p>
-                  <p>{post.date.toDateString()}</p>
+                  <span>{post.description}</span>
+                  <span>{post.date.toDateString()}</span>
                 </CardDescription>
-            </CardHeader>
+              </CardHeader>
               <CardFooter className="justify-center items-center">
                 {post.tags.map((tag: string, index: number) => (
-                  <div key={index}>
-                    <p className="px-2">#{tag}</p>
-                </div>
-              ))}
-            </CardFooter>
-          </Card>
-        </Link>
-      ))}
+                  <span key={index} className="px-2">#{tag}</span>
+                ))}
+              </CardFooter>
+            </Card>
+          </Link>
+        ))}
       </div>
-
-
-
     </main>
   );
 }
