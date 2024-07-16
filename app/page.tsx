aa0b560f9d23152d41379e78d78cb1a27a38d08a
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-07-16 02:10:34 +0000

    Remove Old + Theme Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import { BasicIntro } from '@/components/BasicIntro'
-import { Hero } from '@/components/Hero'
-
 export default function Home() {
   return (
-    <main className=''>
-      <Hero />
-      <BasicIntro />
+    <main>
     </main>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,17 +5,14 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./Icons";
+import { NameThemeChanger } from "@/components/NameThemeChanger";
 
 export function NavBar() {
   return (
     <header className="top-0 z-10 sticky bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b border-border w-full">
       <div className="flex justify-between items-center max-w-screen-2xl h-14 container">
         <div className="flex justify-start items-center space-x-2">
-          <Link href="/">
-            <div className={cn(buttonVariants({ variant: "ghost" }))}>
-              <span>Arian Izadi</span>
-            </div>
-          </Link>
+          <NameThemeChanger />
         </div>
         <div className="flex flex-1 justify-center space-x-3">
           <Link href="/" className={cn(buttonVariants({ variant: "ghost" }))}>
@@ -29,19 +26,19 @@ export function NavBar() {
           <nav className="flex items-center space-x-2">
             <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
               <div className={cn(buttonVariants({ variant: "ghost" }), "w-10 px-0")}>
-                <Icons.Github className="w-4 h-4" />
+                <Icons.Github className="w-4 h-4 fill-black dark:fill-white" />
                 <span className="sr-only">Github</span>
               </div>
             </Link>
             <Link href={siteConfig.links.x} target="_blank" rel="noreferrer">
               <div className={cn(buttonVariants({ variant: "ghost" }), "w-10 px-0")}>
-                <Icons.X className="w-4 h-4" />
+                <Icons.X className="w-4 h-4 fill-black dark:fill-white" />
                 <span className="sr-only">X (Twitter)</span>
               </div>
             </Link>
             <Link href={siteConfig.links.linkedin} target="_blank" rel="noreferrer">
               <div className={cn(buttonVariants({ variant: "ghost" }), "w-10 px-0")}>
-                <Icons.Linkedin className="w-4 h-4" />
+                <Icons.Linkedin className="w-4 h-4 fill-black dark:fill-white" />
                 <span className="sr-only">Linkedin</span>
               </div>
             </Link>
