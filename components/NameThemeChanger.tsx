d548a3297b8fdefcdc4c6269448180ceb5aca19a
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-07-16 02:38:02 +0000

    Update Button Border

--- components/NameThemeChanger.tsx
+++ components/NameThemeChanger.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -18,7 +18,7 @@ export function NameThemeChanger() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="w-fit px-5" size="icon">
+        <Button variant="ghost" className="w-fit px-5" size="icon">
           <div className="flex gap-2">
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
