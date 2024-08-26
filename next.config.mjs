116357106db6ab4bb9eac450af5b779ccb4753b2
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-08-26 14:40:01 -0700

    S3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'arian-next-blog-assets.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
