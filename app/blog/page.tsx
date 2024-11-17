4e73ec067661b6c4bb782c1dcf524614fee53ab9
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-11-17 13:33:17 -0800

    Prefetch Newer Posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -17,11 +17,11 @@ export default async function Home() {
   return (
     <main className="px-4 pt-20 pb-4">
       <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {posts.map((post: BlogPost) => (
-          <Link key={post.id} href={"/blog/" + post.id}>
+        {posts.map((post: BlogPost, index: number) => (
+          <Link key={post.id} href={"/blog/" + post.id} prefetch={index < 3}>
             <Card>
               <CardHeader className="items-center">
-                <CardTitle>{post.title}</CardTitle>
+                <CardTitle>{post.title} - {index}</CardTitle>
                 <CardDescription className="flex flex-col items-center gap-2 pt-3 text-center">
                   <span>{post.description}</span>
                   <span>{post.date.toDateString()}</span>
